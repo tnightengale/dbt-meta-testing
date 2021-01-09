15b624cc0f12912d6211ca94ce4e5ef722b34519
Teghan Nightengale <tnightengale@gmail.com>
2021-01-09 11:54:40 -0500

    edit dbt_project.yml

--- macros/utils/required_tests/evaluate_required_tests.sql
+++ macros/utils/required_tests/evaluate_required_tests.sql
@@ -7,12 +7,12 @@
     {% set tests_per_model = _tests_per_model() %}
     {% set errors = [] %}
 
+    {{ logger("models_to_evaluate: " ~ models_to_evaluate | map(attribute="name") | list) }}
     {% for model in models_to_evaluate %}
-    {{ logger(model) }}
 
         -- If required_tests is dictionary
         {% if model.config.required_tests is mapping %}
-        {{ logger("if reached") }}
+        {{ logger(model.name ~ " if reached") }}
 
 
             {% for test_key in model.config.required_tests.keys() %}
@@ -23,10 +23,13 @@
 
                 {% set provided_test_count = tests_per_model[full_model][test_key] | length %}
 
-                {{ logger(provided_test_count) }}
-
                 {% set required_test_count = model.config.required_tests[test_key] %}
 
+                {{ logger(
+                    "test_key_loop | test_key: " ~ test_key ~ 
+                    " model: " ~ model.name ~
+                    " provided_test_count: " ~ provided_test_count ~
+                    " required_test_count: " ~ required_test_count) }}
 
                 {% if provided_test_count < required_test_count %} 
                     {% do errors.append(

fc596ffdf0539b6bb1d50d65a94726540f3e3736
Teghan Nightengale <tnightengale@gmail.com>
2021-05-03 21:23:19 -0400

    add regex test filters

--- macros/utils/required_tests/evaluate_required_tests.sql
+++ macros/utils/required_tests/evaluate_required_tests.sql
@@ -7,60 +7,31 @@
     {# /*
     Evaluate if each model meets +required_tests minimum.
     */ #}
+    
     {% set tests_per_model = dbt_meta_testing.tests_per_model() %}
     {% set test_errors = [] %}
 
-
-
-    {{ dbt_meta_testing.logger("models_to_evaluate: " ~ models_to_evaluate | map(attribute="name") | list) }}
     {% for model in models_to_evaluate %}
+        {% for test_key in model.config.required_tests.keys() %}
 
-        -- If required_tests is dictionary
-        {% if model.config.required_tests is mapping %}
-        {{ dbt_meta_testing.logger(model.name ~ " if reached") }}
+            {% set provided_test_list = tests_per_model[model.unique_id] %}
 
-            {% for test_key in model.config.required_tests.keys() %}
-
-                -- If the model has less tests than required by the config
-                {% set full_model = model.unique_id %}
-                
-                {{ dbt_meta_testing.logger('tests per model: ' ~ tests_per_model) }}
-
-                -- models that are not declared in properties files will not have keys in tests_per_model
-                {% set provided_test_count = tests_per_model.get(full_model, {}).get(test_key, []) | length %}
-                {{ dbt_meta_testing.logger('provided_test_count: ' ~ provided_test_count) }}
-
-                {% set required_test_count = model.config.required_tests[test_key] %}
-
-                {{ dbt_meta_testing.logger(
-                    "test_key_loop | test_key: " ~ test_key ~ 
-                    " model: " ~ model.name ~
-                    " provided_test_count: " ~ provided_test_count ~
-                    " required_test_count: " ~ required_test_count) }}
-
-                {% if provided_test_count < required_test_count %} 
-
-                    {% do test_errors.append((model.name, test_key, provided_test_count, required_test_count)) %}
-
-                {% endif %}
+            {% set required_test_count = model.config.required_tests[test_key] %}
+            {% set matching_test_count = dbt_meta_testing.get_regex_match_count(provided_test_list, test_key) %}
             
-            {% endfor %}
-        
-        {% endif %}
-
+            {% if matching_test_count < required_test_count %} 
+                {% do test_errors.append((model.name, test_key, matching_test_count, required_test_count)) %}
+            {% endif %}
+            
+        {% endfor %}
     {% endfor %}
 
 
     {% if test_errors | length > 0 %}
-
         {% set result = dbt_meta_testing.error_required_tests(test_errors) %}
-
     {% else %}
-
         {% set result = none %}
-
     {% endif %}
-
 
     {{ return(result) }}
 
